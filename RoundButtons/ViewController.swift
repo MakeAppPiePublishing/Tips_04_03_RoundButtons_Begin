4a16fe291523f7d3f10fd9f3c3f5d94c84c46bf6
Steve Lipton <steve@makeapppie.com>
2018-08-19 12:07:27 -0500

    Initial commit of begin state

--- RoundButtons/ViewController.swift
+++ RoundButtons/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var squareButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
